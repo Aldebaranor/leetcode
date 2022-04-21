89d96db4e356e591a0a6ba77d8070e1deb3b0c56
Aldebaranor <2659460681@qq.com>
2022-04-21 15:07:29 +0800

    leetcode319

---
+++ 319.灯泡开关.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=319 lang=java
+ *
+ * [319] 灯泡开关
+ */
+
+// @lc code=start
+class Solution {
+    public int bulbSwitch(int n) {
+        
+        int light = (int) Math.pow(n, 0.5);
+        return light;
+    }
+}
+// @lc code=end
+
