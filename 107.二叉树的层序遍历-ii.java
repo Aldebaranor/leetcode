355972fa588c5bf9d4ca0fd02eec2eb8db6ec9c2
WangZhisong <2659460681@qq.com>
2023-04-20 09:45:49 +0800

    20230420

---
+++ 107.二叉树的层序遍历-ii.java
@@ -0,0 +1,59 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=107 lang=java
+ *
+ * [107] 二叉树的层序遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if(root!=null){
+            queue.add(root);
+        }
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0;i < size;i++){
+                TreeNode t = queue.poll();
+                level.add(t.val);
+                if(t.left!=null){
+                    queue.add(t.left);
+                }
+                if(t.right!=null){
+                    queue.add(t.right);
+                }
+            }
+            res.add(level);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
+// @lc code=end
+
--- 93.复原-ip-地址.java
+++ 93.复原-ip-地址.java
@@ -14,43 +14,32 @@ class Solution {
     int[] net = new int[4];
 
     public List<String> restoreIpAddresses(String s) {
-        if(s.length()<4||s.length()>12){
+        List<String> res = new ArrayList<>();
+        if (s.length() > 12 || s.length() < 4) {
             return res;
         }
-        dfs(s,0,0);
+        dfs(res, s, "", 0, 0);
         return res;
     }
 
-    public void dfs(String s,int level,int pos){
-        if(level==4){
-            if(pos==s.length()){
-                StringBuffer ipAddr = new StringBuffer();
-                for(int i = 0; i< 4;i++){
-                    ipAddr.append(net[i]);
-                    if(i!=3){
-                        ipAddr.append(".");
-                    }
-                }
-                res.add(ipAddr.toString());
-            }
+    public void dfs(List<String> res, String s, String str, int level, int pos){
+        if (level == 4 && pos == s.length()) {
+            res.add(str);
             return;
         }
-        if(pos==s.length()){
+        if ((4 - level) * 3 < s.length() - pos) {
             return;
         }
-        if(s.charAt(pos)=='0'){
-            net[level]=0;
-            dfs(s, level+1, pos+1);
-        }
-        int addr=0;
-
-        for(int end = pos;end < s.length();end++){
-            addr = addr *10 +(s.charAt(end)-'0');
-            if(addr>0&&addr<=255){
-                net[level]=addr;
-                dfs(s, level+1, pos+1);
-            }else{
+        int addr = 0;
+        for (int i = pos; i < s.length(); i++) {
+            if (s.charAt(pos) == '0' && i > pos) {
                 break;
+            }
+            addr = addr * 10 + (s.charAt(i) - '0');
+            if (addr <= 255 && addr >= 0) {
+                dfs(res, s, str+addr+((level==3)?"":"."), level + 1, i + 1);
+            } else {
+                return;
             }
         }
 
